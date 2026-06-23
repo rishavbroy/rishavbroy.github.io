9d77394d1515bdea200ed9d70ec57064a6076edf
Rishav Roy <roybrishav@gmail.com>
2026-06-23 18:43:09 -0500

    Expand people registry for course instructors

--- src/data/people.ts
+++ src/data/people.ts
@@ -1,7 +1,7 @@
 export interface Person {
   id: string;
   name: string;
-  url: string;
+  url?: string;
 }
 
 export const people: Person[] = [
@@ -35,7 +35,6 @@ export const people: Person[] = [
     name: "Shengwen Gan",
     url: "https://sites.google.com/view/shengwengan/"
   },
-
   {
     id: "frederic-sala",
     name: "Frederic Sala",
@@ -65,6 +64,134 @@ export const people: Person[] = [
     id: "simeon-alder",
     name: "Simeon Alder",
     url: "https://users.ssc.wisc.edu/~sdalder/"
+  },
+  {
+    id: "yong-cai",
+    name: "Yong Cai",
+    url: "https://yong-cai.github.io/"
+  },
+  {
+    id: "jack-porter",
+    name: "Jack Porter",
+    url: "https://users.ssc.wisc.edu/~jporter1/"
+  },
+  {
+    id: "daniel-quint",
+    name: "Daniel Quint",
+    url: "https://users.ssc.wisc.edu/~dquint/"
+  },
+  {
+    id: "benjamin-bernard",
+    name: "Benjamin Bernard",
+    url: "https://benjamin-bernard.com/"
+  },
+  {
+    id: "lones-smith",
+    name: "Lones Smith",
+    url: "https://lonessmith.com/"
+  },
+  {
+    id: "marzena-rostek",
+    name: "Marzena Rostek",
+    url: "https://users.ssc.wisc.edu/~rostek/"
+  },
+  {
+    id: "dean-corbae",
+    name: "Dean Corbae",
+    url: "https://sites.google.com/a/wisc.edu/deancorbae/"
+  },
+  {
+    id: "kenneth-hendricks",
+    name: "Kenneth Hendricks",
+    url: "https://users.ssc.wisc.edu/~khendricks2/"
+  },
+  {
+    id: "jean-francois-houde",
+    name: "Jean-François Houde",
+    url: "https://jfhoude.econ.wisc.edu/"
+  },
+  {
+    id: "gevorg-mnatsakanyan",
+    name: "Gevorg Mnatsakanyan"
+  },
+  {
+    id: "john-gillett",
+    name: "John Gillett",
+    url: "https://stat.wisc.edu/staff/gillett-john/"
+  },
+  {
+    id: "bi-cheng-wu",
+    name: "Bi Cheng Wu",
+    url: "https://stat.wisc.edu/staff/wu-bi-cheng/"
+  },
+  {
+    id: "sean-paul",
+    name: "Sean T. Paul",
+    url: "https://people.math.wisc.edu/~stpaul/"
+  },
+  {
+    id: "david-hansen",
+    name: "David Hansen",
+    url: "https://econ.wisc.edu/staff/hansen-david/"
+  },
+  {
+    id: "ivan-ermakoff",
+    name: "Ivan Ermakoff",
+    url: "https://sociology.wisc.edu/staff/ermakoff-ivan/"
+  },
+  {
+    id: "corina-mommaerts",
+    name: "Corina Mommaerts",
+    url: "https://sites.google.com/site/corinamommaerts/"
+  },
+  {
+    id: "agustin-gutierrez",
+    name: "Agustín Gutiérrez",
+    url: "https://www.gutierrezagustin.com/"
+  },
+  {
+    id: "taiya-bach",
+    name: "Taiya Bach",
+    url: "https://www.linkedin.com/in/taiya-bach-86441525/"
+  },
+  {
+    id: "jeremy-foltz",
+    name: "Jeremy Foltz",
+    url: "https://aae.wisc.edu/faculty/jdfoltz/"
+  },
+  {
+    id: "b-ian-hutchins",
+    name: "B. Ian Hutchins",
+    url: "https://ischool.wisc.edu/staff/hutchins-b-ian/"
+  },
+  {
+    id: "jeremy-mclaughlin",
+    name: "Jeremy L. McLaughlin",
+    url: "https://www.linkedin.com/in/jeremy-l-mclaughlin-mlis-9a99402/"
+  },
+  {
+    id: "veronika-kusumaryati",
+    name: "Veronika Kusumaryati",
+    url: "https://www.anthropology.wisc.edu/staff/kusumaryati-veronika/"
+  },
+  {
+    id: "eduardo-schmidt-passos",
+    name: "Eduardo Schmidt Passos",
+    url: "https://csld.wisc.edu/staff/schmidt-passos-eduardo/"
+  },
+  {
+    id: "zaidan-wu",
+    name: "Zaidan Wu"
+  },
+  {
+    id: "gurmail-singh",
+    name: "Gurmail Singh",
+    url: "https://pages.cs.wisc.edu/~gurmail-singh/Gurmail.html"
+  },
+  {
+    id: "joseph-bowling",
+    name: "Joseph Bowling",
+    url: "https://english.wisc.edu/staff/bowling-joseph/"
   }
 ];
 
